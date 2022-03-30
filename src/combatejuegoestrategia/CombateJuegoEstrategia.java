48c33094e8cd73a101de0d51208bd7f96634902f
Cydonia8 <holymustaine20@gmail.com>
2022-03-30 09:34:54 +0200

    Calculo de atributos

--- src/combatejuegoestrategia/CombateJuegoEstrategia.java
+++ src/combatejuegoestrategia/CombateJuegoEstrategia.java
@@ -24,6 +24,10 @@ public class CombateJuegoEstrategia {
         int[]jugador2 = new int[atributos.length];
         String nombre_p1, nombre_p2;
         
+        int[]ponderaciones_moradores={12,10,8,25,22,20,10,7,5,17,15,12,15,13,12};
+        int[]ponderaciones_puros={15,12,7,8,6,4,20,18,18,40,38,36,17,15,14};
+        int[]ponderaciones_guardianes={12,10,8,35,33,31,16,14,12,9,7,6,13,11,11};
+        
         //Introducción de datos del primer personaje
         do{
             System.out.println("¿Qué nombre quieres para tu personaje? ");
@@ -119,124 +123,136 @@ public class CombateJuegoEstrategia {
         }
         
         //Configuración de los atributos para el primer personaje INCLUIR UN ARRAY CON LOS LÍMITES PARA CADA PARÁMETRO
+        //Estas variables controlan los índices de los arrays de ponderaciones de cada clase
+        int j= 0;
+        int k = 1;
+        int z = 2;
         
         for (int i = 0; i < atributos.length; i++) {
             switch(clase_p1){
                 case "Moradores del Sendero":
                     if(puntos_j1[i] >= 10 && puntos_j1[i] <= 44){
-                        jugador1[0] += puntos_j1[i]/12;
-                        jugador1[1] += puntos_j1[i]/12;
-                        jugador1[2] += puntos_j1[i] /11;
-                        jugador1[3] += 0;
+                        jugador1[0] += puntos_j1[i]/ponderaciones_moradores[j];
+                        jugador1[1] += puntos_j1[i]/ponderaciones_moradores[j];
+                        jugador1[2] += puntos_j1[i] /ponderaciones_moradores[j];
+                        jugador1[3] += puntos_j1[i] /ponderaciones_moradores[j];
                     }else if(puntos_j1[i] >= 45 && puntos_j1[i] <= 74){
-                        jugador1[0] += puntos_j1[i]/10;
-                        jugador1[1] += puntos_j1[i] /10;
-                        jugador1[2] += puntos_j1[i]/9;
-                        jugador1[3] += 0;
+                        jugador1[0] += puntos_j1[i]/ponderaciones_moradores[k];
+                        jugador1[1] += puntos_j1[i] /ponderaciones_moradores[k];
+                        jugador1[2] += puntos_j1[i]/ponderaciones_moradores[k];
+                        jugador1[3] += puntos_j1[i]/ponderaciones_moradores[k];
                     }else{
-                        jugador1[0] += puntos_j1[i]/8;
-                        jugador1[1] += puntos_j1[i]/8;
-                        jugador1[2] += puntos_j1[i]/8;
-                        jugador1[3] += 0;
+                        jugador1[0] += puntos_j1[i]/ponderaciones_moradores[z];
+                        jugador1[1] += puntos_j1[i]/ponderaciones_moradores[z];
+                        jugador1[2] += puntos_j1[i]/ponderaciones_moradores[z];
+                        jugador1[3] += puntos_j1[i]/ponderaciones_moradores[z];
                     }
                     break;
                 case "Puros de Cuerpo y Alma":
                     if(puntos_j1[i] >= 10 && puntos_j1[i] <= 44){
-                        jugador1[0] += puntos_j1[i]/10;
-                        jugador1[1] += puntos_j1[i]/10;
-                        jugador1[2] += puntos_j1[i] /9;
-                        jugador1[3] += 0;
+                        jugador1[0] += puntos_j1[i]/ponderaciones_puros[j];
+                        jugador1[1] += puntos_j1[i]/ponderaciones_puros[j];
+                        jugador1[2] += puntos_j1[i] /ponderaciones_puros[j];
+                        jugador1[3] += puntos_j1[i] /ponderaciones_puros[j];
                     }else if(puntos_j1[i] >= 45 && puntos_j1[i] <= 74){
-                        jugador1[0] += puntos_j1[i]/9;
-                        jugador1[1] += puntos_j1[i] /9;
-                        jugador1[2] += puntos_j1[i]/8;
-                        jugador1[3] += 0;
+                        jugador1[0] += puntos_j1[i]/ponderaciones_puros[k];
+                        jugador1[1] += puntos_j1[i] /ponderaciones_puros[k];
+                        jugador1[2] += puntos_j1[i]/ponderaciones_puros[k];
+                        jugador1[3] += puntos_j1[i]/ponderaciones_puros[k];
                     }else{
-                        jugador1[0] += puntos_j1[i]/7;
-                        jugador1[1] += puntos_j1[i]/7;
-                        jugador1[2] += puntos_j1[i]/7;
-                        jugador1[3] += 0;
+                        jugador1[0] += puntos_j1[i]/ponderaciones_puros[z];
+                        jugador1[1] += puntos_j1[i]/ponderaciones_puros[z];
+                        jugador1[2] += puntos_j1[i]/ponderaciones_puros[z];
+                        jugador1[3] += puntos_j1[i]/ponderaciones_puros[z];
                     }
                     break;
                 case "Guardianes de Saturno":
                     if(puntos_j1[i] >= 10 && puntos_j1[i] <= 44){
-                        jugador1[0] += puntos_j1[i]/12;
-                        jugador1[1] += puntos_j1[i]/12;
-                        jugador1[2] += puntos_j1[i] /11;
-                        jugador1[3] += 0;
+                        jugador1[0] += puntos_j1[i]/ponderaciones_guardianes[j];
+                        jugador1[1] += puntos_j1[i]/ponderaciones_guardianes[j];
+                        jugador1[2] += puntos_j1[i] /ponderaciones_guardianes[j];
+                        jugador1[3] += puntos_j1[i] /ponderaciones_guardianes[j];
                     }else if(puntos_j1[i] >= 45 && puntos_j1[i] <= 74){
-                        jugador1[0] += puntos_j1[i]/10;
-                        jugador1[1] += puntos_j1[i] /10;
-                        jugador1[2] += puntos_j1[i]/9;
-                        jugador1[3] += 0;
+                        jugador1[0] += puntos_j1[i]/ponderaciones_guardianes[k];
+                        jugador1[1] += puntos_j1[i] /ponderaciones_guardianes[k];
+                        jugador1[2] += puntos_j1[i]/ponderaciones_guardianes[k];
+                        jugador1[3] += puntos_j1[i]/ponderaciones_guardianes[k];
                     }else{
-                        jugador1[0] += puntos_j1[i]/8;
-                        jugador1[1] += puntos_j1[i]/8;
-                        jugador1[2] += puntos_j1[i]/8;
-                        jugador1[3] += 0;
+                        jugador1[0] += puntos_j1[i]/ponderaciones_guardianes[z];
+                        jugador1[1] += puntos_j1[i]/ponderaciones_guardianes[z];
+                        jugador1[2] += puntos_j1[i]/ponderaciones_guardianes[z];
+                        jugador1[3] += puntos_j1[i]/ponderaciones_guardianes[z];
                     }
             }
+            j++;
+            k++;
+            z++;
             
         }
         
         //Configuración de los atributos para el segundo personaje
-        
+        j= 0;
+        k = 1;
+        z = 2;
         for (int i = 0; i < atributos.length; i++) {
             switch(clase_p2){
                 case "Moradores del Sendero":
                     if(puntos_j2[i] >= 10 && puntos_j2[i] <= 44){
-                        jugador2[0] += puntos_j2[i]/12;
-                        jugador2[1] += puntos_j2[i]/12;
-                        jugador2[2] += puntos_j2[i] /11;
-                        jugador2[3] += 0;
+                        jugador2[0] += puntos_j2[i]/ponderaciones_moradores[j];
+                        jugador2[1] += puntos_j2[i]/ponderaciones_moradores[j];
+                        jugador2[2] += puntos_j2[i] /ponderaciones_moradores[j];
+                        jugador2[3] += puntos_j2[i] /ponderaciones_moradores[j];
                     }else if(puntos_j2[i] >= 45 && puntos_j2[i] <= 74){
-                        jugador2[0] += puntos_j2[i]/10;
-                        jugador2[1] += puntos_j2[i] /10;
-                        jugador2[2] += puntos_j2[i]/9;
-                        jugador2[3] += 0;
+                        jugador2[0] += puntos_j2[i]/ponderaciones_moradores[k];
+                        jugador2[1] += puntos_j2[i] /ponderaciones_moradores[k];
+                        jugador2[2] += puntos_j2[i]/ponderaciones_moradores[k];
+                        jugador2[3] += puntos_j2[i]/ponderaciones_moradores[k];
                     }else{
-                        jugador2[0] += puntos_j2[i]/8;
-                        jugador2[1] += puntos_j2[i]/8;
-                        jugador2[2] += puntos_j2[i]/8;
-                        jugador2[3] += 0;
+                        jugador2[0] += puntos_j2[i]/ponderaciones_moradores[z];
+                        jugador2[1] += puntos_j2[i]/ponderaciones_moradores[z];
+                        jugador2[2] += puntos_j2[i]/ponderaciones_moradores[z];
+                        jugador2[3] += puntos_j2[i]/ponderaciones_moradores[z];
                     }
                     break;
                 case "Puros de Cuerpo y Alma":
                     if(puntos_j2[i] >= 10 && puntos_j2[i] <= 44){
-                        jugador2[0] += puntos_j2[i]/10;
-                        jugador2[1] += puntos_j2[i]/10;
-                        jugador2[2] += puntos_j2[i] /9;
-                        jugador2[3] += 0;
+                        jugador2[0] += puntos_j2[i]/ponderaciones_puros[j];
+                        jugador2[1] += puntos_j2[i]/ponderaciones_puros[j];
+                        jugador2[2] += puntos_j2[i] /ponderaciones_puros[j];
+                        jugador2[3] += puntos_j2[i] /ponderaciones_puros[j];
                     }else if(puntos_j2[i] >= 45 && puntos_j2[i] <= 74){
-                        jugador2[0] += puntos_j2[i]/9;
-                        jugador2[1] += puntos_j2[i] /9;
-                        jugador2[2] += puntos_j2[i]/8;
-                        jugador2[3] += 0;
+                        jugador2[0] += puntos_j2[i]/ponderaciones_puros[k];
+                        jugador2[1] += puntos_j2[i] /ponderaciones_puros[k];
+                        jugador2[2] += puntos_j2[i]/ponderaciones_puros[k];
+                        jugador2[3] += puntos_j2[i]/ponderaciones_puros[k];
                     }else{
-                        jugador2[0] += puntos_j2[i]/7;
-                        jugador2[1] += puntos_j2[i]/7;
-                        jugador2[2] += puntos_j2[i]/7;
-                        jugador2[3] += 0;
+                        jugador2[0] += puntos_j2[i]/ponderaciones_puros[z];
+                        jugador2[1] += puntos_j2[i]/ponderaciones_puros[z];
+                        jugador2[2] += puntos_j2[i]/ponderaciones_puros[z];
+                        jugador2[3] += puntos_j2[i]/ponderaciones_puros[z];
                     }
                     break;
                 case "Guardianes de Saturno":
                     if(puntos_j2[i] >= 10 && puntos_j2[i] <= 44){
-                        jugador2[0] += puntos_j2[i]/12;
-                        jugador2[1] += puntos_j2[i]/12;
-                        jugador2[2] += puntos_j2[i] /11;
-                        jugador2[3] += 0;
+                        jugador2[0] += puntos_j2[i]/ponderaciones_guardianes[j];
+                        jugador2[1] += puntos_j2[i]/ponderaciones_guardianes[j];
+                        jugador2[2] += puntos_j2[i] /ponderaciones_guardianes[j];
+                        jugador2[3] += puntos_j2[i] /ponderaciones_guardianes[j];
                     }else if(puntos_j2[i] >= 45 && puntos_j2[i] <= 74){
-                        jugador2[0] += puntos_j2[i]/10;
-                        jugador2[1] += puntos_j2[i] /10;
-                        jugador2[2] += puntos_j2[i]/9;
-                        jugador2[3] += 0;
+                        jugador2[0] += puntos_j2[i]/ponderaciones_guardianes[k];
+                        jugador2[1] += puntos_j2[i] /ponderaciones_guardianes[k];
+                        jugador2[2] += puntos_j2[i]/ponderaciones_guardianes[k];
+                        jugador2[3] += puntos_j2[i]/ponderaciones_guardianes[k];
                     }else{
-                        jugador2[0] += puntos_j2[i]/8;
-                        jugador2[1] += puntos_j2[i]/8;
-                        jugador2[2] += puntos_j2[i]/8;
-                        jugador2[3] += 0;
+                        jugador2[0] += puntos_j2[i]/ponderaciones_guardianes[z];
+                        jugador2[1] += puntos_j2[i]/ponderaciones_guardianes[z];
+                        jugador2[2] += puntos_j2[i]/ponderaciones_guardianes[z];
+                        jugador2[3] += puntos_j2[i]/ponderaciones_guardianes[z];
                     }
             }
+            j++;
+            k++;
+            z++;
             
         }
         
